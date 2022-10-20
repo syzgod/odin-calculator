4aeb3a92009277e5d388699a2586f7ec17ba0c72
syzgod <viktor.berczeli@gmail.com>
2022-10-20 14:25:56 +0200

    Operations added

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ const ops = document.querySelector(".operations");
 const clearBtn = document.querySelector(".clear-btn");
 const delBtn = document.querySelector(".del-btn");
 
-let num1;
-let num2;
+let num1, num2, operation;
 
 buttons.forEach((el) =>
   el.addEventListener("click", function (e) {
@@ -15,13 +14,15 @@ buttons.forEach((el) =>
     }
     ops.innerText += e.target.innerText;
     if (e.target.dataset.type === "ops") {
+      operation = e.target.innerText;
+      console.log(operation);
       num1 = +numInput.value;
       numInput.value = "";
       console.log(num1);
     }
     if (e.target.dataset.type === "equal") {
       num2 = +numInput.value;
-      numInput.value = `${num1 + num2}`;
+      numInput.value = doMath();
       ops.innerText += numInput.value;
     }
   })
@@ -38,6 +39,21 @@ delBtn.addEventListener("click", () => {
   numInput.value = numInput.value.slice(0, -1);
   ops.innerText = numInput.value;
 });
+
+function doMath() {
+  if (operation === "+") {
+    return num1 + num2;
+  }
+  if (operation === "-") {
+    return num1 - num2;
+  }
+  if (operation === "*") {
+    return num1 * num2;
+  }
+  if (operation === "/") {
+    return num1 / num2;
+  }
+}
 
 // 1. don't overwrite the number/let add more numbers
 // 2. when the equal pressed use the recognized operation on both of the numbers entered (num1, num2) and return to the input field so as the operation line after the equal sign
