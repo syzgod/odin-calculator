d109bc15d64f5e6bbcfaea2973100d9bd0f92a70
syzgod <viktor.berczeli@gmail.com>
2022-10-21 17:20:30 +0200

    Different JS approach

--- src/index_first_solution.js
+++ src/index_first_solution.js
@@ -2,46 +2,10 @@ const numInput = document.querySelector(".number-input");
 const inputGrid = document.querySelector(".inputs");
 const buttons = document.querySelectorAll("li");
 const ops = document.querySelector(".operations");
-const equalBtn = document.querySelector('[data-type="equal"]');
 const clearBtn = document.querySelector(".clear-btn");
 const delBtn = document.querySelector(".del-btn");
 
 let num1, num2, operation;
-
-function addition(num1, num2) {
-  return num1 + num2;
-}
-
-function subtraction(num1, num2) {
-  return num1 - num2;
-}
-
-function multiplication(num1, num2) {
-  return num1 * num2;
-}
-
-function division(num1, num2) {
-  return num1 / num2;
-}
-
-function operate(operation, num1, num2) {
-  switch (operation) {
-    case "+":
-      addition(num1, num2);
-      break;
-    case "-":
-      subtraction(num1, num2);
-      break;
-    case "/":
-      division(num1, num2);
-      break;
-    case "*":
-      multiplication(num1, num2);
-      break;
-    default:
-      console.log("default");
-  }
-}
 
 buttons.forEach((el) =>
   el.addEventListener("click", function (e) {
@@ -56,25 +20,42 @@ buttons.forEach((el) =>
       numInput.value = "";
       console.log(num1);
     }
+    if (e.target.dataset.type === "equal") {
+      num2 = +numInput.value;
+      numInput.value = doMath();
+      ops.innerText += numInput.value;
+    }
   })
 );
-
-equalBtn.addEventListener("click", (operation, num1) => {
-  num2 = +numInput.value;
-  console.log(num2);
-  numInput.value = operate(operation, num1, num2);
-  ops.innerText += numInput.value;
-});
 
 clearBtn.addEventListener("click", () => {
   numInput.value = "";
   ops.innerText = "";
   num1 = "";
   num2 = "";
-  operation = "";
 });
 
 delBtn.addEventListener("click", () => {
   numInput.value = numInput.value.slice(0, -1);
   ops.innerText = numInput.value;
 });
+
+function doMath() {
+  if (operation === "+") {
+    return num1 + num2;
+  }
+  if (operation === "-") {
+    return num1 - num2;
+  }
+  if (operation === "*") {
+    return num1 * num2;
+  }
+  if (operation === "/") {
+    return num1 / num2;
+  }
+}
+
+// 1. don't overwrite the number/let add more numbers
+// 2. when the equal pressed use the recognized operation on both of the numbers entered (num1, num2) and return to the input field so as the operation line after the equal sign
+// 3. after the calculation has been made disable DEL and also if any new number or operator is entered act accordingly
+// 4. make it work with multiple numbers
